7fbc8986d3b97f1cf9de78ffdc3d819d0cf876de
bibi09456 <55490658+bibi09456@users.noreply.github.com>
2026-04-29 05:24:24 +0700

    Update GeometryDashActivity.kt

--- app/src/main/java/com/geode/launcher/GeometryDashActivity.kt
+++ app/src/main/java/com/geode/launcher/GeometryDashActivity.kt
@@ -509,7 +509,7 @@ class GeometryDashActivity : AppCompatActivity(), Cocos2dxHelper.Cocos2dxHelperL
             }
         }
 
-        if (displayMode == DisplayMode.FULLSCREEN && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        if ((displayMode == DisplayMode.FULLSCREEN || displayMode == DisplayMode.LIMITED) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
             GeodeUtils.handleSafeArea = true
         } else {
